522ed107cbd9975c8f1c5fb3708051d711d8efe2
nikhilnowfloats <nikhil.swami@nowfloats.com>
2023-10-25 18:08:19 +0530

    phase 1 zadinga web

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,30 @@
+importScripts('https://www.gstatic.com/firebasejs/8.4.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.4.1/firebase-messaging.js');
+
+   /*Update with yours config*/
+  const firebaseConfig = {
+    apiKey: "AIzaSyBewnCZ-9S9fNmF0WiTVN2BoTuT3IwE2uk",
+    authDomain: "zadinga-dev.firebaseapp.com",
+    databaseURL: "https://zadinga-dev.firebaseio.com",
+    projectId: "zadinga-dev",
+    storageBucket: "zadinga-dev.appspot.com",
+    messagingSenderId: "1016845239760",
+    appId: "1:1016845239760:web:d778081e790b6c7b8fa5d5",
+    measurementId: "G-L0ZHLESS6Z"
+ };
+  firebase.initializeApp(firebaseConfig);
+  const messaging = firebase.messaging();
+
+  /*messaging.onMessage((payload) => {
+  console.log('Message received. ', payload);*/
+  messaging.onBackgroundMessage(function(payload) {
+    console.log('Received background message ', payload);
+
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+      body: payload.notification.body,
+    };
+
+    self.registration.showNotification(notificationTitle,
+      notificationOptions);
+  });
