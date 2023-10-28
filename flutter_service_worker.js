a11ff03d813292c5853769df67f8ce923b9818ab
nikhilnowfloats <nikhil.swami@nowfloats.com>
2023-10-28 17:00:49 +0530

    bug fixes for flutter items

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,10 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "1d7405192bc16223fe6b71aefe2f44a4",
-"index.html": "64d71df70ec1981e41ca3273fad6b170",
-"/": "64d71df70ec1981e41ca3273fad6b170",
+"index.html": "94846bdbefbaa9ea792bbc6662b90f39",
+"/": "94846bdbefbaa9ea792bbc6662b90f39",
 "firebase-messaging-sw.js": "a15c21cc44cbeeba26549eb879852c7d",
-"main.dart.js": "4accb31dcd357d3f34ca6ff02690263c",
+"main.dart.js": "9166fd934c290de024fa72b4e8986538",
 "sqlite3.wasm": "91f3e6deef838b810d9ee3c8cc750eba",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "sqflite_sw.js": "956c8c891e383d810e98bd8c25f33002",
