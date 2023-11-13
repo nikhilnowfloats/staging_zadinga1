becd98ca46f4bd6b38fc24217e661170f59fd954
nikhilnowfloats <nikhil.swami@nowfloats.com>
2023-11-13 14:30:06 +0530

    deploy latest

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,7 +2,18 @@ importScripts('https://www.gstatic.com/firebasejs/8.4.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.4.1/firebase-messaging.js');
 
    /*Update with yours config*/
-  const firebaseConfig = {
+   firebaseConfig = '${env}' == "production" ?  
+  {
+   apiKey: "AIzaSyCbKKwF1QFS-Iq_MgdZ21NITZyFHncnQQo",
+   authDomain: "zadinga-bca7a.firebaseapp.com",
+   databaseURL: "https://zadinga-bca7a.firebaseio.com",
+   projectId: "zadinga-bca7a",
+   storageBucket: "zadinga-bca7a.appspot.com",
+   messagingSenderId: "944213929604",
+   appId: "1:944213929604:web:8c6cc65a88b3f4f8635cd1",
+   measurementId: "G-2EDKJMZZHZ"
+ }
+  :{
     apiKey: "AIzaSyBewnCZ-9S9fNmF0WiTVN2BoTuT3IwE2uk",
     authDomain: "zadinga-dev.firebaseapp.com",
     databaseURL: "https://zadinga-dev.firebaseio.com",
@@ -14,17 +25,19 @@ importScripts('https://www.gstatic.com/firebasejs/8.4.1/firebase-messaging.js');
  };
   firebase.initializeApp(firebaseConfig);
   const messaging = firebase.messaging();
+  
+  messaging.onBackgroundMessage((payload) =>{
+    console.log('Received background message', payload);
+     // Play audio when a push notification is received
+  const channel = new BroadcastChannel('audio-control');
+  channel.postMessage('play-audio');
 
-  /*messaging.onMessage((payload) => {
-  console.log('Message received. ', payload);*/
-  // messaging.onBackgroundMessage(function(payload) {
-  //   console.log('Received background message ', payload);
-
-  //   const notificationTitle = payload.notification.title;
-  //   const notificationOptions = {
-  //     body: payload.notification.body,
-  //   };
-
-  //   self.registration.showNotification(notificationTitle,
-  //     notificationOptions);
-  // });
+  const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+      body: payload.notification.body,
+      icon: payload.notification.icon,
+    };
+  self.registration.showNotification(notificationTitle,
+      notificationOptions);
+  // channel.close();
+  });
